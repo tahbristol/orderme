41890f7e922d5bee492356ba9962c31c07eaaa61
tahbristol <tahbristol@gmail.com>
2019-05-16 22:59:46 -0400

    user views all orders @ order index

---
+++ spec/features/user_can_view_all_orders_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+feature "Users can view all orders" do
+  scenario "created by anyone" do
+    sign_up('test name', 'test@email.com')
+    create_order(4)
+    
+    click_on "View All Orders"
+    orders = page.all('.orderList .singleOrder')
+    
+    expect(page).to have_content "All Placed Orders"
+    expect(orders.count).to eq 4
+    
+  end
+end
--- spec/support/create_order.rb
+++ spec/support/create_order.rb
@@ -1,12 +1,14 @@
 module Features
-  def create_order
-    click_on 'Create Order'
-    
-    fill_in 'order_line_items_attributes_0_name', with: 'test item'
-    fill_in 'order_line_items_attributes_0_quantity', with: 5
-    fill_in 'order_line_items_attributes_0_price', with: 200
-    fill_in 'order_line_items_attributes_0_catalogue_number', with: 'ABC000'
-    fill_in 'order_line_items_attributes_0_vendor', with: 'test vendor'
-    click_on 'Submit Order'
+  def create_order(number_of_orders_to_create=1)
+    number_of_orders_to_create.times do
+      click_on 'Create Order'
+      
+      fill_in 'order_line_items_attributes_0_name', with: 'test item'
+      fill_in 'order_line_items_attributes_0_quantity', with: 5
+      fill_in 'order_line_items_attributes_0_price', with: 200
+      fill_in 'order_line_items_attributes_0_catalogue_number', with: 'ABC000'
+      fill_in 'order_line_items_attributes_0_vendor', with: 'test vendor'
+      click_on 'Submit Order'
+    end
   end
 end
