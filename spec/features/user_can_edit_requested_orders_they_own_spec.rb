e6762308f1ac97b60e959b6af1dcb13e100aaad4
tahbristol <tahbristol@gmail.com>
2019-05-16 23:20:43 -0400

    failing spec for order edit

---
+++ spec/features/user_can_edit_requested_orders_they_own_spec.rb
@@ -0,0 +1,40 @@
+require 'rails_helper'
+
+feature "User can edit order" do
+  scenario "when they own it and the order status is 'requested'" do
+    sign_up('test name', 'test@email.com')
+    
+    create_order(1)
+    click_on "View All Orders"
+    orders = page.all('.singleOrder')
+    date = orders.first.text
+    click_on date
+    
+    expect(page).to have_content "Edit Order"
+  end
+  
+  scenario "when they own it and the order status is other than requested" do
+    sign_up('test name', 'test@email.com')
+    
+    create_order(1)
+    order = Order.first.update(status: :pending)
+    click_on "View All Orders"
+    orders = page.all('.singleOrder')
+    date = orders.first.text
+    click_on date 
+    
+    expect(page).not_to have_content "Edit Order"
+  end
+  
+  scenario "when they do not own it" do
+    sign_up('test name', 'test@email.com')
+    
+    create_other_users_order(1)
+    click_on "View All Orders"
+    orders = page.all('.singleOrder')
+    date = orders.first.text
+    click_on date
+    
+    expect(page).not_to have_content "Edit Order"
+  end
+end
