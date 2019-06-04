6556255f0613ec9a75b46c8ad2eb54a6089f7dae
samira ahmed <samiraahmed@samiras-MacBook-Pro.local>
2019-06-03 22:54:15 -0400

    add color to flash message

--- app/assets/javascripts/flashNotices.js
+++ app/assets/javascripts/flashNotices.js
@@ -1,7 +1,6 @@
-
-
-document.addEventListener("DOMContentLoaded", function(event) { 
+document.addEventListener("DOMContentLoaded", function(event) {
 	if($('#flashAlerts').text().trim()){
+		$('#flashAlerts').css('background-color', 'lightblue');
 		setTimeout(function(){
 			$('#flashAlerts').animate({ opacity: 0 });
 		},3000);
