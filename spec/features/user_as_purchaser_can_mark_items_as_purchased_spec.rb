eb8f28ea29bb5698c576a6504837d9f3586f40de
Tyler <tahbristol@gmail.com>
2019-06-15 23:28:22 -0400

    mark item as purchased spec

---
+++ spec/features/user_as_purchaser_can_mark_items_as_purchased_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+feature "User as purchaser can mark line items of an order as purchased" do
+	scenario "while an order is pending" do
+		sign_up_as_purchaser('test name', 'test@email.com')
+		create_order(1)
+		visit user_path(User.first)
+		click_on "All Orders"
+		find("#viewOrderListItem_#{Order.first.id}").click
+		click_on "Purchase"
+		click_on "Purchased Queue"
+		find("#viewOrderListItem_#{Order.first.id}").click
+
+		click_on "Begin Ordering"
+
+	end
+end
