d7201b64d5670a29ead569a7faf355f3bf372bc7
tahbristol <tahbristol@gmail.com>
2019-08-24 16:12:46 -0400

    moved order list to partial

--- spec/features/user_can_view_all_orders_spec.rb
+++ spec/features/user_can_view_all_orders_spec.rb
@@ -6,7 +6,7 @@ feature "Users can view all orders" do
     create_order(4)
     
     click_on "All Orders"
-    orders = page.all('.orderList .singleOrder')
+    orders = page.all('.singleOrder')
     
     expect(page).to have_content "Orders" 
     expect(orders.count).to eq 4
