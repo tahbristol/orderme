6bea5253c8b9cc7d2f27cf1fa34b798a7fb099fd
tahbristol <tahbristol@gmail.com>
2019-10-18 14:14:20 -0400

    temp disable search feature

--- app/assets/javascripts/user.js
+++ app/assets/javascripts/user.js
@@ -1,3 +1,6 @@
 document.addEventListener("DOMContentLoaded", function(event) { 
-	
+	let searchButton = document.getElementById('topSearchButton');
+	searchButton.addEventListener('click', function(event){
+		event.preventDefault()
+	})
 });
