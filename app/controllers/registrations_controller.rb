419b0c8de6c17744796f8300d649dbe68123b1f5
tahbristol <tahbristol@gmail.com>
2019-11-08 23:10:59 -0500

    refactor role from signup

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,21 +1,17 @@
 class RegistrationsController < Devise::RegistrationsController
   before_action :configure_permitted_parameters, if: :devise_controller?
-  
+
   def create
-    if params[:user][:role].to_i == 1 # purchaser role
-      params[:user][:role] = 1
-    else
-      params[:user][:role] = 0
-    end
+    params[:user][:role] = params[:user][:role].to_i
     super
   end
-  
+
   protected
 
   def after_sign_up_path_for(resource)
     user_path resource
   end
-  
+
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :email, :password, :role, :picture])
   end
